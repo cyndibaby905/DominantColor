64b2152ed5535a0484c17c0ed7623e6f36d761ae
Emmanuel Odeke <odeke@ualberta.ca>
2014-12-22 00:11:55 -0700

    clib updated & benchmarks added
    To run them:
        > cd clib/
        > make
        > ./alt_lib_test [n] # e.g ./alt_lib_test 1000
    
    Disappointing results though at least using the latest
    lib implementation.

--- DominantColor/clib/alt_lib_test.c
+++ DominantColor/clib/alt_lib_test.c
@@ -3,33 +3,32 @@
 #include <stdlib.h>
 
 #include "alt_lib.h"
-#include "azzert.h"
 
 int main(int argc, char *argv[]) {
-    int *clusters;
+    int *clusterLabels;
     unsigned long int ms;
     size_t k = 16, n = 100;
-    clock_t diff, start = clock();
     DDArray *darr, *centroids;
+    clock_t diff, start = clock();
 
     if (argc >= 2 && sscanf(argv[1], "%zd", &n) != 1) {
         n = 100;
     }
 
-    printf("n: %zd\n", n);
+    printf("\033[92mn: %zd \033[94mk: %zd\033[00m\n", n, k);
 
     darr = populatedDDArray(n, 3);
     centroids = createCentroids(9000, k, darr);
-    clusters = kMeans(darr->data, darr->n, darr->m, centroids->n, 0.01, centroids->data);
+    clusterLabels = kMeans(darr->data, darr->n, darr->m, centroids->n, 0.01, centroids->data);
 
 #ifdef DEBUG
     size_t i;
     for (i = 0; i < k; ++i) {
-        printf("%d\n", clusters[i]); 
+        printf("%d\n", clusterLabels[i]); 
     }
 #endif
 
-    free(clusters);
+    free(clusterLabels);
 
 #ifdef DEBUG
     printDDArray(centroids);
@@ -41,7 +40,7 @@ int main(int argc, char *argv[]) {
 
     diff = clock() - start;
     ms = diff * 1000 / CLOCKS_PER_SEC;
-    printf("Time: %ld ms diff: %ld\n", ms, diff);
+    printf("Timing: %ld ms diff: %ld\n", ms, diff);
 
     return 0;
 }
